992e194b036f55c128e43d41bd23f5e8a40295f9
chenhuiz <chenhui.zhao@helsinki.fi>
2020-03-18 23:18:49 +0200

    Added no solution-situation

--- src/main/java/solver/domain/Nonoblock.java
+++ src/main/java/solver/domain/Nonoblock.java
@@ -19,7 +19,7 @@ public class Nonoblock {
     }
 
     /**
-     * Prints all possible permutations of this block from the given list and
+     * Prints all possible permutations of this block according to the given list and
      * the length of the block. The list of data represents how many and in what
      * order the continuous ones should be in this block. Ones in this case
      * simulate colour grey in a real Nonogram game.
@@ -36,18 +36,22 @@ public class Nonoblock {
         }
 
         List<String> ones = data.stream().map(d -> repeat(d, "1")).collect(toList());
-
-//        List<String> ones = data.stream().map(d -> String.valueOf(1).repeat(d)).collect(toList());
+        
         List<String> sequences = generateSequences(ones, length - cellSum + 1);
-
+        
+        if (sequences.isEmpty()) {
+            io.print("No solution");
+            return;
+        }
+        
         for (String sequence : sequences) {
             io.print(sequence.substring(1));
         }
     }
 
     /**
-     * Generates all possible permutations of this block. The list of ones
-     * represents in which cell should one be.
+     * Generates all possible permutations of this block. 
+     * The list of ones represents in which cell should "1" be.
      *
      * @param ones a list of ones
      * @param numZeros zero numbers in this block
@@ -56,27 +60,26 @@ public class Nonoblock {
     public List<String> generateSequences(List<String> ones, int numZeros) {
         if (ones.isEmpty()) {
             String s = repeat(numZeros, "0");
-//            String s = String.valueOf(0).repeat(numZeros);
-            String str[] = s.split("");
-            return Arrays.asList(str);
+            System.out.println("S: " + s);
+            return Arrays.asList(s);
         }
-
+        
         List<String> result = new ArrayList<>();
         for (int x = 1; x < numZeros - ones.size() + 2; x++) {
             List<String> skipOne = ones.stream().skip(1).collect(toList());
-
+            
             List<String> tails = generateSequences(skipOne, numZeros - x);
-
+            
             for (String tail : tails) {
                 result.add(repeat(x, "0") + ones.get(0) + tail);
+                System.out.println("Result: " + result);
 
-//                result.add(String.valueOf(0).repeat(x) + ones.get(0) + tail);
             }
         }
         return result;
     }
 
-    public String repeat(int n, String s) {
+    private String repeat(int n, String s) {
         String result = "";
         for (int i = 0; i < n; i++) {
             result += s;
